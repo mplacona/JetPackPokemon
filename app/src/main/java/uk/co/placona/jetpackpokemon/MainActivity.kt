2c372ed51f82cc6a8c5f73c9cf4cbfb39520c178
Marcos Placona <marcos.placona@gmail.com>
2019-08-14 11:35:17 +0100

    :sparkles: Add ViewModels for Lifecycle control

--- app/src/main/java/uk/co/placona/jetpackpokemon/MainActivity.kt
+++ app/src/main/java/uk/co/placona/jetpackpokemon/MainActivity.kt
@@ -1,22 +1,14 @@
 package uk.co.placona.jetpackpokemon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import androidx.core.view.ViewCompat.setNestedScrollingEnabled
-import androidx.recyclerview.widget.DefaultItemAnimator
-
-
 
 class MainActivity : AppCompatActivity() {
-
-    private val pokeClient: PokeClient = PokeClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,17 +18,12 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
 
-        val pokemons = pokeClient.getPokemon()
-        pokemons.enqueue(object : Callback<Pokemon> {
-            override fun onResponse(call: Call<Pokemon>, response: Response<Pokemon>) {
-                val pokemonList: List<Result> = response.body()!!.results
-                recyclerView.adapter = PokemonAdapter(pokemonList)
-            }
+        val model = ViewModelProviders.of(this).get(PokemonViewModel::class.java)
 
-            override fun onFailure(call: Call<Pokemon>, t: Throwable) {
-                Log.d("ERROR", "Failure " + t.message)
-            }
-        })
+        model.getPokemons().observe(this,
+            Observer<List<Result>> { pokemonList ->
+                recyclerView.adapter = PokemonAdapter(pokemonList)
+            })
 
     }
 }
